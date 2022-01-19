11364bb12a18d531d1ae5b940f7cea6d56c335ce
alvaro-freire <alvaro.freirea@udc.es>
2022-01-19 21:04:18 +0100

    read files

---
+++ files/open2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#_*_ coding: utf8 _*_
+
+# r = read
+
+archivo = open("wordlist.lst", 'r')
+
+# print(archivo.readlines())
+
+# read lines of a file: (op1)
+#
+# for l in archivo.read().split('\n'):
+#     print(l)
+
+
+# (op2)
+lista = archivo.read().split('\n')
+print(len(lista))
+
+for n in lista:
+    print(n)
