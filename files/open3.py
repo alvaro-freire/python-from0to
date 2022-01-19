d5f620ec02e612edc078d7773b40ea5ab0cdbe9c
alvaro-freire <alvaro.freirea@udc.es>
2022-01-19 21:12:43 +0100

    add content to file

---
+++ files/open3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#_*_ coding: utf8 _*_
+
+# a = append
+
+archivo = open('archivo1.txt', 'a')
+dedicacion = input("Dedicación: ")
+empresa = input("Empresa: ")
+idioma = input("Idioma: ")
+
+archivo.write(dedicacion)
+archivo.write("\n")
+archivo.write(empresa)
+archivo.write("\n")
+archivo.write(idioma)
+archivo.write("\n")
+
+archivo.close()
