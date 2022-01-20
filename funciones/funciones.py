7572fba6fdd4eac1aef9d5dbcc488ec088c3ad10
alvaro-freire <alvaro.freirea@udc.es>
2022-01-20 14:00:29 +0100

    functions in python

---
+++ funciones/funciones.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#_*_ coding: utf8 _*_
+
+
+# __name__
+# main()
+
+def despedida():
+    print("Adiós")
+
+def saludo():
+    print("Hola")
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+    saludo()
+    despedida()
