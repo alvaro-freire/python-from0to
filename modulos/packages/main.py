3d43bb1062a233690b66ddf46694a0a5ab64cf7b
alvaro-freire <alvaro.freirea@udc.es>
2022-02-10 19:11:57 +0100

    packages

---
+++ modulos/packages/main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+#_*_ coding: utf8 _*_
+
+from package1 import mod_calc as calc
+
+def main():
+    print("Hola\n")
+    list = [1, 2, 3, 4]
+    while True:
+        print("1.- Suma dos enteros")
+        print("2.- Resta dos enteros")
+        print("3.- Divide dos enteros")
+        print("4.- Multiplica dos enteros")
+        print("5.- Salir")
+        opcion = int(input("Opción: "))
+
+        if opcion == 5:
+            calc.despedida()
+            exit()
+
+        if not list.__contains__(opcion):
+            print("Opción inválida")
+        else:
+            v1 = int(input("Valor 1: "))
+            v2 = int(input("Valor 2: "))
+            if opcion == 1:
+                calc.add(v1, v2)
+            elif opcion == 2:            
+                calc.sub(v1, v2)
+            elif opcion == 3:
+                calc.divide(v1, v2)
+            else:
+                calc.multiply(v1, v2)
+            
+
+
+if __name__ == '__main__':
+    main()
---
+++ modulos/packages/package1/mod_calc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#_*_ coding: utf8 _*_
+
+
+def add(v1, v2):
+    print("\nResultado: {}\n".format(v1+v2))
+
+def sub(v1, v2):
+    print("\nResultado: {}\n".format(v1-v2))
+
+def multiply(v1, v2):
+    print("\nResultado: {}\n".format(v1*v2))
+
+def divide(v1, v2):
+    print("\nResultado: {}\n".format(v1/v2))
+
+def despedida():
+    print("Adiós!\n")
